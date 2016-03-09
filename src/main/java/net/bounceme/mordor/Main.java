361b0d3dd7aba85f2c883b6cb8ce6e98dcbab6b6
thufir <thufir@mordor.bounceme.net>
2016-03-09 02:40:18 -0800

    init

---
+++ src/main/java/net/bounceme/mordor/Main.java
@@ -0,0 +1,15 @@
+package net.bounceme.mordor;
+
+import static java.lang.System.out;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().foo();
+    }
+
+    private void foo() {
+        out.println("hello world");
+    }
+
+}
